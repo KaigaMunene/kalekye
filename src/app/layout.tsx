344a4a96e69c4bb65fc70b1f2964238f41b15b9c
Kaiga <92078057+KaigaMunene@users.noreply.github.com>
2025-12-15 21:30:54 +0300

    feat(project): create footer (#3)
    * creater footer
    * make additional changes on navabar
    * fix eslint errors by removing undefided lines

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import Navbar from '../components/Navbar';
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-});
+import Footer from '../components/Footer';
 
 export const metadata: Metadata = {
   title: 'KM Network',
@@ -25,9 +15,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="min-h-screen flex flex-col">
         <Navbar />
-        {children}
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
